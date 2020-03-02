7fcf11a74f7c781091664d039076f9d2643e62ba
littleredhat1997 <1656704949@qq.com>
2020-03-03 02:46:29 +0800

    https://github.com/wucao/natx

---
+++ nat-client/src/main/java/handler/LocalProxyHandler.java
@@ -0,0 +1,43 @@
+package handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import protocol.NatxMessage;
+import protocol.NatxMessageType;
+
+import java.util.HashMap;
+
+/**
+ * Created by wucao on 2019/3/2.
+ */
+public class LocalProxyHandler extends NatxCommonHandler {
+
+    private NatxCommonHandler proxyHandler;
+    private String remoteChannelId;
+
+    public LocalProxyHandler(NatxCommonHandler proxyHandler, String remoteChannelId) {
+        this.proxyHandler = proxyHandler;
+        this.remoteChannelId = remoteChannelId;
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        byte[] data = (byte[]) msg;
+        NatxMessage message = new NatxMessage();
+        message.setType(NatxMessageType.DATA);
+        message.setData(data);
+        HashMap<String, Object> metaData = new HashMap<>();
+        metaData.put("channelId", remoteChannelId);
+        message.setMetaData(metaData);
+        proxyHandler.getCtx().writeAndFlush(message);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        NatxMessage message = new NatxMessage();
+        message.setType(NatxMessageType.DISCONNECTED);
+        HashMap<String, Object> metaData = new HashMap<>();
+        metaData.put("channelId", remoteChannelId);
+        message.setMetaData(metaData);
+        proxyHandler.getCtx().writeAndFlush(message);
+    }
+}
